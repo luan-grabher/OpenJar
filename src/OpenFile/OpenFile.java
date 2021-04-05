fdc1a98d1df46d91bfa30a22fd7f99597a8bbf1b
TI01 <TI01@TI01.MORESCO.LOCAL>
2021-04-05 17:48:23 -0300

    added test method to open another jar now work

--- src/OpenFile/OpenFile.java
+++ src/OpenFile/OpenFile.java
@@ -1,23 +1,56 @@
 package OpenFile;
 
+import fileManager.FileManager;
 import java.awt.Desktop;
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 public class OpenFile {
 
+    private static String folder = "\\\\heimerdinger\\docs\\Informatica\\Programas\\Moresco\\02 - Arquivos de Programas\\";
+
     public static void main(String[] args) {
         try {
-            
-            
-            //text file, should be opening in default text editor
-            File file = new File("./Test.jar");
-            
-            Desktop desktop = Desktop.getDesktop();
-            if (file.exists()) {
-                System.out.println(file.getCanonicalPath());
-                Process proc = Runtime.getRuntime().exec("java -jar \"" + file.getAbsolutePath() + "\" ");
+            if (args != null && args.length > 0) {
+                List<String> listArgs = new ArrayList<>(Arrays.asList(args));
+                String program = listArgs.get(0);
+                //Remove o primeiro
+                listArgs.remove(0);
+
+                //text file, should be opening in default text editor
+                File file = new File(folder + program + ".jar");
+
+                //Se o arquivo existir
+                if (file.exists()) {
+                    Calendar cal = Calendar.getInstance();
+                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+                    String text = "";
+                    String date = df.format(cal.getTime());
+                    String user = System.getProperty("user.name");
+
+                    File usages = new File(folder + "programas_usados.csv");
+                    if (usages.exists()) {
+                        text = FileManager.getText(usages);
+                    }
+
+                    text = date + ";" + user + ";" + program + "\r\n" + text;
+                    if (FileManager.save(usages, text)) {
+                        Process proc = Runtime.getRuntime().exec("java -jar \"" + file.getAbsolutePath() + "\"" + (listArgs.size() == 0 ? "" : " " + String.join(" ", listArgs)));
+                    }
+
+                } else {
+
+                    System.out.println("Arquivo não existe");
+                }
             } else {
-                System.out.println("Arquivo não existe");
+                System.out.println("Sem argumentos");
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ test/Test.java
@@ -0,0 +1,11 @@
+
+import OpenFile.OpenFile;
+
+
+public class Test {
+    public static void main(String[] args) {
+        String[] argumentos = new String[]{"Test","ar","gu","men","to"};
+        
+        OpenFile.main(argumentos);
+    }
+}
